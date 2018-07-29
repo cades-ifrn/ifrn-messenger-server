bf9e104eafc1606218084c33dd705f1f8ba2143b
Felipe Pontes <felipemfpontes@gmail.com>
2018-07-29 17:37:59 -0300

    Add .editorconfig
    Issue: #2

--- lib/ifrnmessenger_web/services/suap_service.ex
+++ lib/ifrnmessenger_web/services/suap_service.ex
@@ -1,61 +1,66 @@
-defmodule CommunityWeb.SuapService do 
-    defp url(endpoint) do
-        "https://suap.ifrn.edu.br/api/v2" <> endpoint
-    end
+defmodule CommunityWeb.SuapService do
+  defp url(endpoint) do
+    "https://suap.ifrn.edu.br/api/v2" <> endpoint
+  end
 
-    defp parse_object(body) do
-        body
-          |> Poison.decode!
-          |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
-    end
+  defp parse_object(body) do
+    body
+    |> Poison.decode!()
+    |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
+  end
 
-    defp parse_list(body) do
-        body
-          |> Poison.decode!
-          |> Enum.map(fn(x) -> Enum.map(x, fn({k, v}) -> {String.to_atom(k), v} end) end)
-    end
-    
-    def token(username, password) do
-        body = Poison.encode!(%{
-            "username" => username,
-            "password" => password,
-        })
+  defp parse_list(body) do
+    body
+    |> Poison.decode!()
+    |> Enum.map(fn x -> Enum.map(x, fn {k, v} -> {String.to_atom(k), v} end) end)
+  end
 
-        {:ok, response} = HTTPoison.post(
-            url("/autenticacao/token/"),
-            body,
-            [{"Content-Type", "application/json"}]
-        )
-        
-        data = parse_object(response.body)
+  def token(username, password) do
+    body =
+      Poison.encode!(%{
+        "username" => username,
+        "password" => password
+      })
 
-        data[:token]
-    end
+    {:ok, response} =
+      HTTPoison.post(
+        url("/autenticacao/token/"),
+        body,
+        [{"Content-Type", "application/json"}]
+      )
 
-    def about(token) do
-        {:ok, response} = HTTPoison.get(
-            url("/minhas-informacoes/meus-dados/"),
-            [{"Content-Type", "application/json"}, {"Authentication", "Bearer "<>token}]
-        )
+    data = parse_object(response.body)
 
-        parse_object(response.body)
-    end
+    data[:token]
+  end
 
-    def semesters(token) do
-        {:ok, response} = HTTPoison.get(
-            url("/minhas-informacoes/meus-periodos-letivos/"),
-            [{"Content-Type", "application/json"}, {"Authentication", "Bearer "<>token}]
-        )
+  def about(token) do
+    {:ok, response} =
+      HTTPoison.get(
+        url("/minhas-informacoes/meus-dados/"),
+        [{"Content-Type", "application/json"}, {"Authentication", "Bearer " <> token}]
+      )
 
-        parse_list(response.body)
-    end
+    parse_object(response.body)
+  end
 
-    def classes(token, year, semester) do
-        {:ok, response} = HTTPoison.get(
-            url("/minhas-informacoes/turmas-virtuais/"<>year<>"/"<>semester<>"/"),
-            [{"Content-Type", "application/json"}, {"Authentication", "Bearer "<>token}]
-        )
+  def semesters(token) do
+    {:ok, response} =
+      HTTPoison.get(
+        url("/minhas-informacoes/meus-periodos-letivos/"),
+        [{"Content-Type", "application/json"}, {"Authentication", "Bearer " <> token}]
+      )
 
-        parse_list(response.body)
-    end
+    parse_list(response.body)
+  end
+
+  def classes(token, year, semester) do
+    {:ok, response} =
+      HTTPoison.get(
+        url("/minhas-informacoes/turmas-virtuais/" <> year <> "/" <> semester <> "/"),
+        [{"Content-Type", "application/json"}, {"Authentication", "Bearer " <> token}]
+      )
+
+    parse_list(response.body)
+  end
 end
