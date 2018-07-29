ed6d02652e00db70d09baa6b016bfe166229dabd
Felipe Pontes <felipemfpontes@gmail.com>
2018-07-29 17:31:28 -0300

    Add more services into SuapService
    - about: information about an user
    - semesters: semesters of an user
    - classes: classes per semester of an user
    
    Issue: #2

--- lib/ifrnmessenger_web/services/suap_service.ex
+++ lib/ifrnmessenger_web/services/suap_service.ex
@@ -2,6 +2,18 @@ defmodule CommunityWeb.SuapService do
     defp url(endpoint) do
         "https://suap.ifrn.edu.br/api/v2" <> endpoint
     end
+
+    defp parse_object(body) do
+        body
+          |> Poison.decode!
+          |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+    end
+
+    defp parse_list(body) do
+        body
+          |> Poison.decode!
+          |> Enum.map(fn(x) -> Enum.map(x, fn({k, v}) -> {String.to_atom(k), v} end) end)
+    end
     
     def token(username, password) do
         body = Poison.encode!(%{
@@ -15,11 +27,35 @@ defmodule CommunityWeb.SuapService do
             [{"Content-Type", "application/json"}]
         )
         
-        data = response.body
-          |> Poison.decode!
-          |> Map.take(~w(token))
-          |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+        data = parse_object(response.body)
 
         data[:token]
+    end
+
+    def about(token) do
+        {:ok, response} = HTTPoison.get(
+            url("/minhas-informacoes/meus-dados/"),
+            [{"Content-Type", "application/json"}, {"Authentication", "Bearer "<>token}]
+        )
+
+        parse_object(response.body)
+    end
+
+    def semesters(token) do
+        {:ok, response} = HTTPoison.get(
+            url("/minhas-informacoes/meus-periodos-letivos/"),
+            [{"Content-Type", "application/json"}, {"Authentication", "Bearer "<>token}]
+        )
+
+        parse_list(response.body)
+    end
+
+    def classes(token, year, semester) do
+        {:ok, response} = HTTPoison.get(
+            url("/minhas-informacoes/turmas-virtuais/"<>year<>"/"<>semester<>"/"),
+            [{"Content-Type", "application/json"}, {"Authentication", "Bearer "<>token}]
+        )
+
+        parse_list(response.body)
     end
 end
